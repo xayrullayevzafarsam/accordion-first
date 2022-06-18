4ecefa0b3f332368c42a62b277b5ae07cbdaa566
xayrullayevzafarsam <xayrullayevzafar49@gmail.com>
2022-06-18 18:47:14 +0500

    did accordion

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+let button = document.getElementsByClassName("btn");
+
+let text = document.getElementsByClassName("content");
+
+for ( let i = 0; i <= button.length; i++ ) {
+
+  button[i].onclick = function() {
+
+    for ( let k = 0; k < button.length; k++) {
+
+      if (k == i) {
+
+      } else {
+        text[k].classList.remove("show");
+      }
+    }
+
+
+    text[i].classList.toggle("show");
+  }
+}
